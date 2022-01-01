b6239f32844918ce00b36331307eda03987af48b
SeoeunBae <enyoupark@gmail.com>
2022-01-01 11:14:16 +0900

    basic setting:wrench:

--- src/test/java/study/querydsl/QuerydslApplicationTests.java
+++ src/test/java/study/querydsl/QuerydslApplicationTests.java
@@ -1,13 +1,38 @@
 package study.querydsl;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import study.querydsl.entity.Hello;
+import study.querydsl.entity.QHello;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
 
 @SpringBootTest
+@Transactional
 class QuerydslApplicationTests {
+
+//	@PersistenceContext
+	@Autowired
+	EntityManager em;
 
 	@Test
 	void contextLoads() {
+		Hello hello = new Hello();
+		em.persist(hello);
+
+		JPAQueryFactory jpaQueryFactory = new JPAQueryFactory(em);
+		QHello qHello = new QHello("h");
+
+		Hello hello1 = jpaQueryFactory.selectFrom(qHello)
+				.fetchOne();
+
+		Assertions.assertThat(hello1).isEqualTo(hello);
+
 	}
 
 }
